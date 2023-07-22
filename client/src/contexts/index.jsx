5937c6801bc6d200706373aa7e9726a547bc71c7
Hussein Raji <111huraji@gmail.com>
2023-07-22 18:12:00 +0100

    use axios instead of fetch

--- client/src/contexts/index.jsx
+++ client/src/contexts/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext, createContext, useEffect } from "react";
 import { useNavigate } from "react-router";
+import axios from "axios";
 
 const AuthContext = createContext();
 
@@ -10,37 +11,38 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const getUserDataByToken = async () => {
       const token = localStorage.getItem("token");
-      //if no token, user must also be null
+      //if token is undefined, user must also be null
       if (!token) {
         setUser(null);
         return;
       }
 
-      const options = {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          authorization: token,
-        },
-      };
-
-      const response = await fetch(`${process.env.SERVER}/users`, options);
-      const data = await response.json();
-
-      if (response.statusCode === 200) {
-        setUser(data);
-      } else {
-        //if token exists but the fetch request returns anything other than a 200 status code the token is invalid or the token never existed in the db at all
-        alert("Couldn't get user information or your session has expired");
-        localStorage.removeItem("token");
-        setTimeout(() => goTo("/login"), 2000);
+      try {
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        const { status, data } = await axios.get(
+          `${process.env.SERVER}/users`,
+          config,
+        );
+        if (status === 200) {
+          setUser(data);
+        } else {
+          //if token exists but the fetch request returns anything other than a 200 status code the token is invalid or the token never existed in the db at all
+          alert("Your session has expired");
+          localStorage.removeItem("token");
+          setTimeout(() => goTo("/login"), 2000);
+        }
+      } catch (error) {
+        console.log(error);
       }
     };
 
     getUserDataByToken();
   }, [localStorage.getItem("token")]);
-    
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       {children}
