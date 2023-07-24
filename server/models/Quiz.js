56601db6ea14679d763dbaa3c0443b6c49ee3bc1
ajearle11 <ajearle9@gmail.com>
2023-07-24 23:33:24 +0100

    Fixed issue with post request

--- server/models/Quiz.js
+++ server/models/Quiz.js
@@ -98,7 +98,7 @@ class Quiz {
     );
 
     if (response.rows.length != 1) {
-      throw new Error("No Entry available for this user");
+      return "No Entry available for this user";
     }
 
     const quiz = response.rows[0];
@@ -115,31 +115,41 @@ class Quiz {
       language_id,
     } = data;
 
-    const response = await db.query(
-      "INSERT INTO Quizzes(quiz_id, user_id, beginner_score, intermediate_score, advanced_score, language_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-      [
-        quiz_id,
-        user_id,
-        beginner_score,
-        intermediate_score,
-        advanced_score,
-        language_id,
-      ]
-    );
-
-    if (response.rows.length != 1) {
-      throw new Error("Unable to add quiz");
-    }
-
-    //get quiz by quiz id where language id = language from above
-    const newQuiz = response.rows[0];
-    const newEntry = await Quiz.getAllInfoForOneUser(
+    const checkIfThisQuizExists = await Quiz.getAllInfoForOneUser(
       user_id,
       language_id,
       quiz_id
     );
 
-    return new Quiz(newEntry);
+    if (checkIfThisQuizExists.quiz_instance > 0) {
+      throw new Error("Quiz already exists");
+    } else if (checkIfThisQuizExists === "No Entry available for this user") {
+      const response = await db.query(
+        "INSERT INTO Quizzes(quiz_id, user_id, beginner_score, intermediate_score, advanced_score, language_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+        [
+          quiz_id,
+          user_id,
+          beginner_score,
+          intermediate_score,
+          advanced_score,
+          language_id,
+        ]
+      );
+
+      if (response.rows.length != 1) {
+        throw new Error("Unable to add quiz");
+      }
+
+      //get quiz by quiz id where language id = language from above
+      const newQuiz = response.rows[0];
+      const newEntry = await Quiz.getAllInfoForOneUser(
+        user_id,
+        language_id,
+        quiz_id
+      );
+
+      return new Quiz(newEntry);
+    }
   }
 
   async updateQuizInstance(data) {
