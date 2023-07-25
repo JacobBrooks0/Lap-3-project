20204fdd6e9910508bd3f02fd016adfbb39a72ae
Jacob <Jacobabrooks04@gmail.com>
2023-07-25 09:38:41 +0100

    test

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 import { useAuth } from "../../contexts";
+import { Link } from 'react-router-dom';
 
 import style from "./style.module.css";
 
 export default function Dashboard() {
+  
   const { user } = useAuth();
+  const [difficulty, setDifficulty] = useState([]);
 
   return (
     <>
       <main id="dashboard" className={style["container"]}>
-        <div />
-        <div />
-        <div />
-        <div>
-          <div>Profile</div>
-          <h1>Well done!</h1>
-          <h2>You made it {user}!</h2>
-          <h2>🙌</h2>
-        </div>
+          <div className={style["practice"]}>
+            <Link to="/learn">
+              <div id="practice1" className={style["practice"]}>
+                <h1>Practice 1</h1>
+              </div>
+            </Link>
+            <Link to="/learn">
+            <div id="practice2" >
+              <h1>Practice 2</h1>
+            </div>
+            </Link>
+          </div>
+          <div>
+            <Link to="quiz">
+            <div>Beginner</div>
+            </Link>
+            <Link to="quiz">
+            <div>Intermediate</div>
+            </Link>
+            <Link to="quiz">
+            <div>Advanced</div>
+            </Link>
+            
+          </div>
       </main>
     </>
   );
